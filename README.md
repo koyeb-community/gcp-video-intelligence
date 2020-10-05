39cf0f3ae67bc2dd1e3d0478801cd5e15f1d4b21
Edouard BONLIEU <ebonlieu@gmail.com>
2020-10-05 15:30:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+---
+display_name: GCP video intelligence
+icon: "https://assets.koyeb.com/icons/gcp.svg"
+short_description: The GCP video intelligence function allows you to enable powerful content discovery and analysis in your videos.
+status: ACTIVE
+version: 1.0.0
+---
+
+## Summary
+
+GCP video intelligence is a video detection service allowing you to use Google video analysis technology for your applications.
+The GCP video intelligence function allows you to detect persons, faces, labels, and logos in videos.
+
+This function comes with the following variants:
+
+- **Video label detection**, to identify objects, locations, activities, animal species, products, and more.
+- **Video logo recognition**, to recognize the presence of over 100,000 brands and logos in video content.
+**Video person detection**, to detect humans' presence in a video and track the bounding box of individual people across the video.
+- **Video face detection**, to detect faces and face attribute in a video. Attributes contain information about headwear, frontal gaze, eye visibility, glasses, mouth, and smile.
+
+## Advantages of using Koyeb's GCP video intelligence Function
+
+With Koyeb, you can run the GCP Video intelligence integration to add video analysis and content discovery to your applications in seconds:
+
+- Store the result on the cloud service provider of your choice
+- Ready to use, no code or complex configuration required to get started
+- Combine additional processing actions that immediately use the results of the GCP video intelligence function
+
+## Popular Use Cases with the GCP video intelligence Function
+
+- Perform content moderation in videos
+- Deliver contextual advertisements
+- Build a recommendation engine
+- Media classification and archiving
+
+## How the GCP video intelligence Function Works with Koyeb
+
+To get started with the GCP video intelligence function, you have to copy the configuration snippet below and replace the required values with yours.
+This function is triggered each time an object of type `.mp4, .mpeg4, .mov, or .avi` is created in the Store you use.
+
+Below is what you need to use this function:
+
+* A Koyeb valid account
+* Valid GCP service account configuration with permissions to access GCP video intelligence service
+* A Store to upload videos and save the function result
---
+++ gcp-video-intelligence.js
@@ -0,0 +1,154 @@
+/** @format */
+
+"use strict";
+const VideoIntelligence = require("@google-cloud/video-intelligence").v1p3beta1;
+const S3 = require("aws-sdk/clients/s3");
+
+const videoIntelligenceFeatures = {
+  LABEL_DETECTION: {
+    responseScope: "segmentLabelAnnotations",
+  },
+  FACE_DETECTION: {
+    responseScope: "faceDetectionAnnotations",
+  },
+  LOGO_RECOGNITION: {
+    responseScope: "logoRecognitionAnnotations",
+  },
+  PERSON_DETECTION: {
+    responseScope: "personDetectionAnnotations",
+  },
+  SPEECH_TRANSCRIPTION: {
+    responseScope: "speechTranscriptions",
+  },
+};
+
+const prefix =
+  process.env.VIDEO_INTELLIGENCE_FILE_REFIX || "gcp-video-intelligence";
+
+const executeVideoIntelligenceFeature = async (
+  videoIntelligenceInstance,
+  s3Instance,
+  objectbBody,
+  key,
+  bucket
+) => {
+  try {
+    const videoIntelligenceFeature =
+      process.env.VIDEO_INTELLIGENCE_FEATURE || "LABEL_DETECTION";
+
+    const obj = await s3Instance
+      .getObject({
+        Bucket: bucket,
+        Key: key,
+      })
+      .promise();
+
+    const request = {
+      inputContent: obj.Body.toString("base64"),
+      features: [videoIntelligenceFeature],
+      videoContext: {
+        speechTranscriptionConfig: {
+          languageCode: "en-US",
+          enableAuto: true,
+        },
+      },
+    };
+
+    if (!(videoIntelligenceFeature in videoIntelligenceFeatures)) {
+      throw new Error("Uknown video intelligence feature.");
+    }
+
+    const [operation] = await videoIntelligenceInstance.annotateVideo(request);
+
+    const results = await operation.promise();
+
+    const resultsData =
+      results[0].annotationResults[0][
+        videoIntelligenceFeatures[videoIntelligenceFeature].responseScope
+      ];
+
+    await s3Instance
+      .upload({
+        Bucket: bucket,
+        Key: `${prefix}-${videoIntelligenceFeature}-${key}.json`,
+        Body: JSON.stringify({ results: resultsData }, null, 4),
+        ContentType: "application/json",
+      })
+      .promise();
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getSourceObject = async (s3Instance, bucket, key) => {
+  try {
+    const response = await s3Instance
+      .getObject({
+        Bucket: bucket,
+        Key: key,
+      })
+      .promise();
+
+    return response.Body;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getS3Configuration = (sourceBucket) => {
+  return {
+    accessKeyId: process.env[`KOYEB_STORE_${sourceBucket}_ACCESS_KEY`],
+    secretAccessKey: process.env[`KOYEB_STORE_${sourceBucket}_SECRET_KEY`],
+    region: process.env[`KOYEB_STORE_${sourceBucket}_REGION`],
+    endpoint: process.env[`KOYEB_STORE_${sourceBucket}_ENDPOINT`],
+  };
+};
+
+const validateEnvironment = (sourceBucket) => {
+  if (!sourceBucket) {
+    throw new Error("Bucket name not present in event payload.");
+  }
+
+  if (
+    !process.env?.[`KOYEB_STORE_${sourceBucket}_ACCESS_KEY`] ||
+    !process.env?.[`KOYEB_STORE_${sourceBucket}_SECRET_KEY`] ||
+    !process.env[`KOYEB_STORE_${sourceBucket}_REGION`] ||
+    !process.env[`KOYEB_STORE_${sourceBucket}_ENDPOINT`]
+  ) {
+    throw new Error(
+      `One of the following environment variables are missing: KOYEB_STORE_${sourceBucket}_ACCESS_KEY, KOYEB_STORE_${sourceBucket}_SECRET_KEY, KOYEB_STORE_${sourceBucket}_ENDPOINT, KOYEB_STORE_${sourceBucket}_REGION.`
+    );
+  }
+
+  if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+    throw new Error(
+      "Environment variables GOOGLE_APPLICATION_CREDENTIALS must be set."
+    );
+  }
+};
+
+const handler = async (event) => {
+  const bucket = event?.bucket?.name;
+  const key = event?.object?.key;
+
+  if (key.startsWith(prefix)) {
+    return;
+  }
+
+  validateEnvironment(bucket);
+
+  const s3Instance = new S3(getS3Configuration(bucket));
+  const videoIntelligenceInstance = new VideoIntelligence.VideoIntelligenceServiceClient();
+
+  const objectbBody = await getSourceObject(s3Instance, bucket, key);
+
+  await executeVideoIntelligenceFeature(
+    videoIntelligenceInstance,
+    s3Instance,
+    objectbBody,
+    key,
+    bucket
+  );
+};
+
+module.exports.handler = handler;
